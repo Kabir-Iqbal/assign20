9248e70cdc4a40dcfe55b3447796af92da584df9
Kabir-iqbal <161919126+Kabir-Iqbal@users.noreply.github.com>
2024-04-03 03:09:10 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,8 @@
+// 20. Think of something you could store in an array. For example, you could make a list of mountains, 
+// rivers, countries, cities, languages, or anything else you’d like. Write a program that creates a list 
+// containing these items.
+let cities :string []= ["NewYork","Tokyo","Berlin","Denver","Moscow","Nairobi"]
+cities.forEach(fvrtcity => {
+    console.log(fvrtcity);
+    
+});
